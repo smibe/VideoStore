c494f120845dca543320c2a7202c9199069d1edb
Gerhard <geh@geherbert.de>
2013-06-27 04:40:31 +0200

    Using a table

--- VideoStore.Tests/VideoStoreTests.cs
+++ VideoStore.Tests/VideoStoreTests.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using RefactoringDemo;
+using VideoStore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -10,43 +10,66 @@ namespace Tests
     [TestClass]
     public class VideoStoreTests
     {
-        private Customer customer;
+        private Statement statement;
 
         [TestInitialize]
         public void SetUp()
         {
-            customer = new Customer("Fred");
+            statement = new Statement("Fred");
         }
 
         [TestMethod]
         public void SingleNewReleaseStatement()
         {
-            customer.AddRental(new Rental(new Movie("The Cell", Movie.PriceCode.NEW_RELEASE), 3));
-            Assert.AreEqual("Rental Record for Fred\n\tThe Cell\t9\nAmount owed is 9\nYou earned 2 frequent renter points", customer.Statement());
+            statement.AddRental(new Rental(new Movie("The Cell", Movie.PriceCode.NEW_RELEASE), 3));
+            Assert.AreEqual(
+                "Rental Record for Fred\n" +
+                "\tThe Cell\t9\nAmount owed is 9\n" +
+                "You earned 2 frequent renter points",
+                statement.Generate());
+            Assert.AreEqual(9.0, statement.TotalAmount);
+            Assert.AreEqual(2, statement.FrequentPoints);
         }
 
         [TestMethod]
         public void DualNewReleaseStatement()
         {
-            customer.AddRental(new Rental(new Movie("The Cell", Movie.PriceCode.NEW_RELEASE), 3));
-            customer.AddRental(new Rental(new Movie("The Tigger Movie", Movie.PriceCode.NEW_RELEASE), 3));
-            Assert.AreEqual("Rental Record for Fred\n\tThe Cell\t9\n\tThe Tigger Movie\t9\nAmount owed is 18\nYou earned 4 frequent renter points", customer.Statement());
+            statement.AddRental(new Rental(new Movie("The Cell", Movie.PriceCode.NEW_RELEASE), 3));
+            statement.AddRental(new Rental(new Movie("The Tigger Movie", Movie.PriceCode.NEW_RELEASE), 3));
+            statement.Generate();
+            Assert.AreEqual(18.0, statement.TotalAmount);
+            Assert.AreEqual(4, statement.FrequentPoints);
         }
 
         [TestMethod]
         public void SingleChildrensReleaseStatement()
         {
-            customer.AddRental(new Rental(new Movie("The Tigger Movie", Movie.PriceCode.CHILDRENS), 3));
-            Assert.AreEqual("Rental Record for Fred\n\tThe Tigger Movie\t1,5\nAmount owed is 1,5\nYou earned 1 frequent renter points", customer.Statement());
+            statement.AddRental(new Rental(new Movie("The Tigger Movie", Movie.PriceCode.CHILDRENS), 3));
+            statement.Generate();
+            Assert.AreEqual(1.5, statement.TotalAmount);
+            Assert.AreEqual(1, statement.FrequentPoints);
         }
 
         [TestMethod]
-        public void MultipleRegularStatement()
+        public void MultipleRegularStatementFormat()
         {
-            customer.AddRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.PriceCode.REGULAR), 1));
-            customer.AddRental(new Rental(new Movie("8 1/2", Movie.PriceCode.REGULAR), 2));
-            customer.AddRental(new Rental(new Movie("Eraserhead", Movie.PriceCode.REGULAR), 3));
-            Assert.AreEqual("Rental Record for Fred\n\tPlan 9 from Outer Space\t2\n\t8 1/2\t2\n\tEraserhead\t3,5\nAmount owed is 7,5\nYou earned 3 frequent renter points", customer.Statement());
+            statement.AddRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.PriceCode.REGULAR), 1));
+            statement.AddRental(new Rental(new Movie("8 1/2", Movie.PriceCode.REGULAR), 2));
+            statement.AddRental(new Rental(new Movie("Eraserhead", Movie.PriceCode.REGULAR), 3));
+            statement.Generate();
+            Assert.AreEqual("Rental Record for Fred\n\tPlan 9 from Outer Space\t2\n\t8 1/2\t2\n\tEraserhead\t3,5\nAmount owed is 7,5\nYou earned 3 frequent renter points", statement.Generate());
+        }
+
+
+        [TestMethod]
+        public void MultipleRegularStatementValue()
+        {
+            statement.AddRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.PriceCode.REGULAR), 1));
+            statement.AddRental(new Rental(new Movie("8 1/2", Movie.PriceCode.REGULAR), 2));
+            statement.AddRental(new Rental(new Movie("Eraserhead", Movie.PriceCode.REGULAR), 3));
+            statement.Generate();
+            Assert.AreEqual(7.5, statement.TotalAmount);
+            Assert.AreEqual(3, statement.FrequentPoints);
         }
     }
 }
--- VideoStore/Rental.cs
+++ VideoStore/Rental.cs
@@ -2,28 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VideoStore;
 
-namespace RefactoringDemo
+namespace VideoStore
 {
+    public enum MovieType
+    {
+        Regular = 0,
+        NewRelease = 1,
+        ChildrensMovie = 2,
+    }
+
+    public struct MovieRentalRules
+    {
+        public Func<int, double> calculateAmount;
+        public Func<int, int> calculateFrequentRenterPoints;
+
+        public MovieRentalRules(Func<int, double> calculateAmount, Func<int, int> calculateFrequentRenterPoints)
+        {
+            this.calculateAmount = calculateAmount;
+            this.calculateFrequentRenterPoints = calculateFrequentRenterPoints;
+        }
+    }
+
     public class Rental
     {
         private Movie movie;
         private int daysRented;
+        private MovieRentalRules[] rentalRules;
 
         public Rental(Movie movie, int daysRented)
         {
             this.movie = movie;
             this.daysRented = daysRented;
+
+            this.rentalRules = new MovieRentalRules[3];
+            rentalRules[(int)MovieType.Regular] = new MovieRentalRules((days) => 2 + ((days > 2) ? (days - 2) * 1.5 : 0), (days) => 1);
+            rentalRules[(int)MovieType.NewRelease] = new MovieRentalRules((days) => days * 3, (days) => days > 1 ? 2 : 1);
+            rentalRules[(int)MovieType.ChildrensMovie] = new MovieRentalRules((days) => 1.5 + ((days > 3) ? (days - 3) * 1.5 : 0), (days) => 1);
         }
 
-        public int GetDaysRented()
+        public Movie Movie
         {
-            return daysRented;
+            get
+            {
+                return this.movie;
+            }
         }
 
-        public Movie GetMovie()
+        public double GetAmount()
         {
-            return movie;
+            MovieRentalRules rentalRule = this.rentalRules[(int)this.movie.GetPriceCode()];
+            return rentalRule.calculateAmount(this.daysRented);
+        }
+
+        public int GetFrequentRenterPoints()
+        {
+            MovieRentalRules rentalRule = this.rentalRules[(int)this.movie.GetPriceCode()];
+            return rentalRule.calculateFrequentRenterPoints(this.daysRented);
         }
     }
 }
